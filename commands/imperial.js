e2f64fca08f2d97b9b09b966e9a94a749ff742e3
nzfs <fonseca.nazareno@gmail.com>
2023-01-13 23:11:57 -0500

    agregar imperial

---
+++ commands/imperial.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('discord.js');
+const request = require('request-promise');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('imperial')
+		.setDescription('Replies with an imperial gif'),
+	async execute(interaction) {
+		const imageUrl = 'https://nzfs.net/img/imperial.gif';
+		const imageBuffer = await request({
+			url: imageUrl,
+			encoding: null,
+		});
+		//await interaction.reply({ files: [{ attachment: imageBuffer, name: 'tpdne.jpg' }] });
+		await interaction.reply('Imperial');
+		await interaction.channel.send({ files: [{ attachment: imageBuffer, name: 'imperial.jpg' }] });
+	},
+};
